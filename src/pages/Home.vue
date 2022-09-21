5485c42ceea2a04e78e27d8fa77513abb5266782
Minkyu3 <gkhtls2006@broadwave.co.kr>
2022-09-21 18:00:21 +0900

    axios 설치 + 백앤데에서 데이터 받기

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,7 +4,7 @@
     <section class="py-5 text-center container">
       <div class="row py-lg-5">
         <div class="col-lg-6 col-md-8 mx-auto">
-          <h1 class="fw-light">Album example</h1>
+          <h1 class="fw-light">BIGSHOP</h1>
           <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
           <p>
             <a href="#" class="btn btn-primary my-2">Main call to action</a>
@@ -29,9 +29,16 @@
 
 <script>
 import Card from "@/components/Card";
+import axios from "axios";
 export default {
   name: 'Home',
   components: {Card},
+
+  setup(){
+    axios.get('/api/items').then((res) => {
+      console.log(res);
+    })
+  }
 }
 </script>
 
